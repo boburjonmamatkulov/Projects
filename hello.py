3f80ec0f9ed940ecfe760f46af1be69ee05148de
Boburjon <boburjon6838@gmail.com>
2026-06-13 12:10:45 +0500

    first commit

---
+++ hello.py
@@ -0,0 +1,10 @@
+talaba = {
+    "ismi": "Ali",
+    "familiyasi": "Valiyev",
+    "yoshi": 20,
+    "shahar": "Toshkent"
+}
+print(talaba["ismi"])
+print(talaba["shahar"])
+talaba["maktab"] = "15-maktab"
+print(talaba)
